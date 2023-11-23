cf8406e7e8c338aab404493a801c1f08d210df82
Endeley <93675367+Endeley@users.noreply.github.com>
2023-11-23 01:08:15 +0100

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDnFsESgP1S1fqDsndwwGuW8YS6BRikLRs',
+    authDomain: 'house-marketplace-1c0ca.firebaseapp.com',
+    projectId: 'house-marketplace-1c0ca',
+    storageBucket: 'house-marketplace-1c0ca.appspot.com',
+    messagingSenderId: '612788526467',
+    appId: '1:612788526467:web:07975c79518ebc665d2ab6',
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
