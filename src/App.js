40940d3c2118b0ffdb181a4ce623b0b4d2290e2e
Endeley <93675367+Endeley@users.noreply.github.com>
2023-11-24 19:54:25 +0100

    create prfile user update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 //
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
 import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
@@ -14,11 +17,13 @@ function App() {
                     <Route path='/' element={<Explore />} />
                     <Route path='/forgot-password' element={<ForgotPassword />} />
                     <Route path='/offers' element={<Offers />} />
-                    <Route path='/profile' element={<SignIn />} />
-                    <Route path='/signin' element={<SignIn />} />
-                    <Route path='/signup' element={<SignUp />} />
+                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/sign-in' element={<SignIn />} />
+                    <Route path='/sign-up' element={<SignUp />} />
                 </Routes>
+                <Navbar />
             </Router>
+            <ToastContainer />
         </>
     );
 }
